f7d6f2c5e247e5bbe7bad36ec7f99fb9464d9048
Dinesh9418 <dineshsjadhav9418@gmail.com>
2024-03-03 12:39:08 +0530

    Landing page added

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import "./Home.css";
+import Bike  from "../../Assets/Images/bike.png";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="home-main">
+      <div className="home_1">
+        <h1 className="home_1_heading">
+          Easy And Fast Way To <span className="home_1_span">Rent</span> Your
+          Car
+        </h1>
+        <p className="home_1_loream">
+          Lorem Ipsum is simply dummy text of the printing and type setting
+          industry.Lorem Ipsum has been the industry standard dummy text ever
+          since the 1500.
+        </p>
+        <button type="button" class="btn btn-primary">
+          Rent Car
+        </button>
+      </div>
+      <div className="home_2">
+        <img className="home_2_img" src={Bike} alt="bike_img" />
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
