86aef9db19bc3429d8ae0398d9bf59ad5cce10ad
Dinesh9418 <dineshsjadhav9418@gmail.com>
2024-03-04 23:06:07 +0530

    About Page added

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,9 +1,68 @@
-import React from 'react'
+import React from "react";
+import "./About.css";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="about-main">
+      <div className="main-wrapper">
+        <h1>
+          Why <span>Choose</span> Us ?
+        </h1>
+        <p>
+          Neque porro quisquam est qui dolorem ipsum quia dolor sit amet,
+          consectetur,
+        </p>
+      </div>
+      <div className="about-card">
+        <div className="about-card_1">
+          <div>
+            <p>ICON</p>
+          </div>
+          <div>
+            <h3>24 Hrs Support</h3>
+            <p>
+              {" "}
+              Neque porro quisquam est qui dolorem ipsum quia dolor sit amet,
+              consectetur,
+            </p>
+          </div>
+        </div>
+        <div className="about-card_2"><div>
+          <p>ICON</p>
+        </div>
+          <div>
+            <h3>Best Price</h3>
+            <p>
+              {" "}
+              Neque porro quisquam est qui dolorem ipsum quia dolor sit amet,
+              consectetur,
+            </p>
+          </div></div>
+        <div className="about-card_3"><div>
+          <p>ICON</p>
+        </div>
+          <div>
+            <h3>Verified License</h3>
+            <p>
+              {" "}
+              Neque porro quisquam est qui dolorem ipsum quia dolor sit amet,
+              consectetur,
+            </p>
+          </div></div>
+        <div className="about-card_4"><div>
+          <p>ICON</p>
+        </div>
+          <div>
+            <h3>Free Cancellation</h3>
+            <p>
+              {" "}
+              Neque porro quisquam est qui dolorem ipsum quia dolor sit amet,
+              consectetur,
+            </p>
+          </div></div>
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
